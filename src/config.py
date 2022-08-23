358e8c6b12bc3b8e7350b1d083ae0824c7a9ca09
s2652633 <m.a.vasylenko@student.utwente.nl>
2022-08-23 13:42:16 +0200

    added config for temperature ranges

---
+++ src/config.py
@@ -0,0 +1,17 @@
+# Acceptable temperature ranges
+
+# Primary cycle
+
+T_PRI_SUP_LOWER = 0
+T_PRI_SUP_UPPER = 0
+
+T_PRI_RET_LOWER = 0
+T_PRI_RET_UPPER = 0
+
+# Secondary cycle
+
+T_SEC_SUP_LOWER = 0
+T_SEC_SUP_UPPER = 0
+
+T_SEC_RET_LOWER = 0
+T_SEC_RET_UPPER = 0
